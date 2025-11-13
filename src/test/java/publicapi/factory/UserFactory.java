1aeba74a6e09fb87c5ad59ed78f8fdd182444b20
Jussara Souza <jussaragranja19@gmail.com>
2025-11-13 20:28:52 -0300

    support

--- src/test/java/publicapi/factory/UserFactory.java
+++ src/test/java/publicapi/factory/UserFactory.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import publicapi.model.UserModel;
+import publicapi.support.Constants;
 
 import java.util.List;
 
@@ -18,59 +19,18 @@ import static publicapi.support.Constants.*;
 
 public class UserFactory {
 
-    public static JSONObject validBody(int random) {
-        JSONObject userJson = new JSONObject();
-        userJson.accumulate("name", NAME_VALUE);
-        userJson.accumulate("email", "jussarateste"+random+"@gmail.com");
-        userJson.accumulate("gender", GENDER_FEMALE);
-        userJson.accumulate("status", STATUS_ACTIVE);
-        return userJson;
+
+    public static JSONObject createValidUser(int random) {
+        return new JSONObject()
+                .put("name", Constants.NAME_VALUE)
+                .put("email", "jussarateste" + random + "@gmail.com")
+                .put("gender", Constants.GENDER_FEMALE)
+                .put("status", Constants.STATUS_ACTIVE);
     }
 
-    public static JSONObject validBodyEmail(int random) {
-        JSONObject mailJson = new JSONObject();
-        mailJson.accumulate("email", "jussarateste"+random+"@gmail.com");
-        return mailJson;
-    }
-
-    public static List<UserModel> getUser(){
-        List<UserModel> userModelList = given()
-            .header(AUTHORIZATION, BEARER_TOKEN.concat(TOKEN))
-            .contentType(ContentType.JSON)
-        .when()
-            .get(PATH_USERS)
-        .then()
-            .statusCode(HttpStatus.SC_OK)
-            .body(CODE, equalTo(HttpStatus.SC_OK)).extract().jsonPath().getList("data", UserModel.class);
-
-        return userModelList;
-    }
-
-    public static boolean userExist(int id){
-        given()
-            .header(AUTHORIZATION, BEARER_TOKEN.concat(TOKEN))
-            .contentType(ContentType.JSON)
-            .pathParam(ID, id)
-        .when()
-            .get(PATH_USERS_ID)
-        .then()
-            .statusCode(HttpStatus.SC_OK)
-            .body(CODE, equalTo(HttpStatus.SC_OK));
-
-        return true;
-    }
-
-    public static boolean userNotExist(int id){
-        given()
-            .header(AUTHORIZATION, BEARER_TOKEN.concat(TOKEN))
-            .contentType(ContentType.JSON)
-            .pathParam(ID, id)
-        .when()
-            .get(PATH_USERS_ID)
-        .then()
-            .body(CODE, equalTo(HttpStatus.SC_NOT_FOUND));
-
-        return true;
+    public static JSONObject createValidEmail(int random) {
+        return new JSONObject()
+                .put("email", "jussarateste" + random + "@gmail.com");
     }
 
 }
